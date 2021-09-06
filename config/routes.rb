04879a7a3af2fd3496ac4e78ad06b23bccbf3c6c
niikz <i.k307112@gmail.com>
2021-09-06 21:41:50 +0900

    deviseのUserコントローラを作成/ルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "books#index"
+
+  devise_for :users, controllers: {
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
