77d1b2734bbfdbb088448567deba16a41ea73bc9
niikz <i.k307112@gmail.com>
2021-09-11 01:14:16 +0900

    letter_opener_webを導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # letter_opener_web
+  config.action_mailer.delivery_method = :letter_opener_web
 end
