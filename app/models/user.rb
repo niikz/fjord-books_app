3b9da9ca61b116d547075bd070f1904cdc40a554
niikz <i.k307112@gmail.com>
2021-09-20 19:23:14 +0900

    followings関連のメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_many :active_friendships, class_name: 'Friendship', foreign_key: 'follower_id', dependent: :destroy
   has_many :followings, through: :active_friendships, source: :followed
+
+  def follow(other_user)
+    followings << other_user
+  end
+
+  def unfollow(other_user)
+    active_friendships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    followings.include?(other_user)
+  end
 end
