fb6130f7b3053094f57a81b68400a56cfdb1a7f5
niikz <i.k307112@gmail.com>
2021-09-23 23:42:06 +0900

    日報の新規作成メソッドとビューを実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,14 +8,32 @@ class ReportsController < ApplicationController
   def show; end
 
   def new
+    @report = Report.new
   end
 
   def edit
+  end
+
+  def create
+    @report = current_user.reports.new(report_params)
+
+    respond_to do |format|
+      if @report.save
+        format.html { redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human) }
+        format.json { render :show, status: :created, location: @report }
+      else
+        format.html { render :new }
+        format.json { render json: @report.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
 
   def set_report
     @report = Report.find(params[:id])
+  end
+  def report_params
+    params.require(:report).permit(:title, :body)
   end
 end
