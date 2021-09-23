db265e785a07281b89a95fc3244a52d760ff030a
niikz <i.k307112@gmail.com>
2021-09-23 23:28:08 +0900

    日報の詳細メソッドとビューを実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,21 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show]
+
   def index
     @reports = Report.order(:id).page(params[:page])
   end
 
-  def show
-  end
+  def show; end
 
   def new
   end
 
   def edit
+  end
+
+  private
+
+  def set_report
+    @report = Report.find(params[:id])
   end
 end
