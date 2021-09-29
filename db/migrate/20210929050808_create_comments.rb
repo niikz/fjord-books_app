a44665d804e7cbb83a630fb1658261a370665913
niikz <i.k307112@gmail.com>
2021-09-29 14:13:49 +0900

    Commentモデルを作成 Bookモデル、Reportモデルへポリモーフィック関連付け

---
+++ db/migrate/20210929050808_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comments do |t|
+      t.references :commentable, polymorphic: true, null: false
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
