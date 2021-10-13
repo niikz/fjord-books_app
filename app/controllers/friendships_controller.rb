7e6afef3f48750316bafee008d0cd13f95a1a8c2
niikz <i.k307112@gmail.com>
2021-10-13 23:47:44 +0900

    ルーティングヘルパーを使うよう修正

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,12 +4,12 @@ class FriendshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
-    redirect_to user
+    redirect_to user_path(user)
   end
 
   def destroy
     user = Friendship.find(params[:id]).followed
     current_user.unfollow(user)
-    redirect_to user
+    redirect_to user_path(user)
   end
 end
