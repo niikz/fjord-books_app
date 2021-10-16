10264c06e7d24e35a6685f11625b543c600e77be
niikz <i.k307112@gmail.com>
2021-10-16 13:54:46 +0900

    follower_idのindexを削除

--- db/migrate/20210920063628_create_friendships.rb
+++ db/migrate/20210920063628_create_friendships.rb
@@ -8,7 +8,6 @@ class CreateFriendships < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
-    add_index :friendships, :follower_id
     add_index :friendships, :followed_id
     add_index :friendships, %i[follower_id followed_id], unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2021_09_20_063628) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["followed_id"], name: "index_friendships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_friendships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_friendships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
