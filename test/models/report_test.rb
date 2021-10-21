1fa1d6846c023a17be35aaafdce8532c07b1c3a7
niikz <i.k307112@gmail.com>
2021-10-21 16:15:08 +0900

    Reportモデルのテストを実装

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,20 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @alice = users(:alice)
+    @bob = users(:bob)
+    @report = reports(:one)
+  end
+
+  test '#created_on' do
+    travel_to Time.zone.local(2021, 10, 21) do
+      assert_equal '2021/10/21'.to_date, @report.created_on
+    end
+  end
+
+  test '#editable?' do
+    assert_equal false, @report.editable?(@bob)
+    assert_equal true, @report.editable?(@alice)
+  end
 end
